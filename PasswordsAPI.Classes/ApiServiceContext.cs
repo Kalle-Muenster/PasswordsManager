31f385b9eca212e43cf0391289dc2803a1fca0ac
Kalle Muenster <kalle.muenster@outlook.de>
2022-03-22 17:15:11 +0100

    Add abstract base class for DbContext
    which provides accessing dynamically added DbSet instances
    which on the real implemented DbContext inside the main
    application can be handled via DbSet properties which carry
    these DbSet objects Injected by the DependenncyInjector

---
+++ PasswordsAPI.Classes/ApiServiceContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PasswordsAPI.BaseClasses;
+
+
+namespace PasswordsAPI.Database
+{
+    public interface IPasswordaApiDbContext<CTX> where CTX : DbContext
+    {
+        DbSet<MOD> EntitiesSet<MOD>() where MOD : EntityBase<MOD>, new();
+        void SetEntities<MOD>(DbSet<MOD> dbSet ) where MOD : EntityBase<MOD>, new();
+    }
+
+    public abstract class PasswordsApiDbContext<CTX>
+        : DbContext
+        , IPasswordaApiDbContext<CTX>
+        where CTX : PasswordsApiDbContext<CTX>
+    {
+        private Hashtable _dbSets = new Hashtable();
+
+        public PasswordsApiDbContext( DbContextOptions<CTX> options ) : base (options)
+        {}
+
+        public DbSet<MOD> EntitiesSet<MOD>()
+            where MOD : EntityBase<MOD>, new()
+        {
+            return _dbSets[typeof(MOD)] as DbSet<MOD>;
+        }
+
+        public void SetEntities<MOD>( DbSet<MOD> addDbSet )
+            where MOD : EntityBase<MOD>, new()
+        {
+            if ( addDbSet != null )
+                _dbSets[typeof(MOD)] = addDbSet;
+        }
+    }
+}
