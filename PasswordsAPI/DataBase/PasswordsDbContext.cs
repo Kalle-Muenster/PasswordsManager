ddfa0dc92c797a352b89826abc1eb5221cd13d2a
Kalle Muenster <kalle.muenster@outlook.de>
2022-03-22 17:20:33 +0100

    Move the DbContext implementation to Main App
    ...from the Models module where it was implemented before

---
+++ PasswordsAPI/DataBase/PasswordsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordsAPI.Models;
+using PasswordsAPI.Services;
+
+namespace PasswordsAPI.Database
+{
+    public class PasswordsDbContext : PasswordsApiDbContext<PasswordsDbContext>
+    {
+        public PasswordsDbContext(DbContextOptions<PasswordsDbContext> options)
+            : base(options)
+        { }
+
+        public virtual DbSet<PasswordUsers> PasswordUsers
+        {
+            get { return EntitiesSet<PasswordUsers>();}
+            set { SetEntities(value); }
+        }
+        public virtual DbSet<UserPasswords> UserPasswords
+        {
+            get { return EntitiesSet<UserPasswords> (); }
+            set { SetEntities(value); }
+        }
+        public virtual DbSet<UserLocations> UserLocations
+        {
+            get { return EntitiesSet<UserLocations>(); }
+            set { SetEntities(value); }
+        }
+    }
+}
