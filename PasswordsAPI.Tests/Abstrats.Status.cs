e6023d0326a7adc3c3bab4abb8eb6505acb9995c
Kalle Muenster <kalle.muenster@outlook.de>
2022-03-24 10:42:47 +0100

    Rename Tests -> Abstracts.Tests

--- PasswordsAPI.Tests/Abstrats.Status.cs
+++ PasswordsAPI.Tests/Abstrats.Status.cs
@@ -5,13 +5,6 @@ namespace PasswordsAPI.Tests
 {
     public class BaseClasses
     {
-        private readonly Status Success = Status.Success;
-        private readonly Status NoState = Status.NoError;
-        private readonly Status Invalid = Status.Invalid;
-        private readonly Status Unknown = Status.Unknown;
-        private readonly Status Cryptic = Status.Cryptic;
-        private readonly Status Service = Status.Service;
-
         private readonly Status CriticalCaseOne = new Status(ResultCode.Invalid | ResultCode.Success);
         private readonly Status CriticalCaseTwo = new Status(ResultCode.Unknown | ResultCode.Success);
 
@@ -24,16 +17,16 @@ namespace PasswordsAPI.Tests
             Assert.False(nostate.Bad, nostate); // if nothing there IS, then there's nothing Bad
             Assert.False(nostate.Ok, nostate); // if nothing there IS, for what is it Good for?
             Assert.False(nostate.Waiting, nostate); // as long nothing has been initiated, nothing awaited will happen (e.g. no cause - no effect)
-            Assert.True(nostate.ToString().StartsWith("Status"), nostate.ToString());
+            Assert.True(nostate.ToString().StartsWith("Status"), nostate);
         }
 
         public void AssertSuccessCase(Status success)
         {
-            Assert.True(success, success.ToString()); // passes silently, sure
+            Assert.True(success, success ); // passes silently, sure
             Assert.False(success.Bad, success); // no, it's ok
             Assert.True(success.Ok, success); // yes that it is
             Assert.False(success.Waiting, success); // for what at all?
-            Assert.True(success.ToString().StartsWith("Success"),success.ToString());
+            Assert.True(success.ToString().StartsWith("Success"),success);
         }
 
         public void AssertUnknownCase(Status unknown)
@@ -42,54 +35,61 @@ namespace PasswordsAPI.Tests
             Assert.True(unknown.Bad, unknown); // I don't know if it's ok,... so could be maybe Bad
             Assert.False(unknown.Ok, unknown); // one cannot say it would be ok for sure yet..
             Assert.True(unknown.Waiting, unknown); // not yet evaluable, so wait till gets known
-            Assert.True(unknown.ToString().StartsWith("Status"), unknown.ToString());
+            Assert.True(unknown.ToString().StartsWith("Status"), unknown);
         }
 
         public void AssertInvalidCase(Status invalid)
         {
-            Assert.False(invalid, invalid.ToString()); // should not pass silently
+            Assert.False(invalid, invalid ); // should not pass silently
             Assert.True(invalid.Bad, invalid); // is 'Bad' in any case
             Assert.False(invalid.Ok, invalid); // is NOT Ok at all
             Assert.False(invalid.Waiting, invalid); // No, it's already evaluated being 'Invalid'
-            Assert.True(invalid.ToString().StartsWith("Error"),invalid.ToString());
+            Assert.True(invalid.ToString().StartsWith("Error"), invalid );
         }
 
         [Fact]
         public void BasicStatusValues()
         {
-            AssertNoStateCase(NoState);
-            AssertSuccessCase(Success);
-            AssertUnknownCase(Unknown);
-            AssertInvalidCase(Invalid);
+            AssertNoStateCase(Status.NoState);
+            AssertSuccessCase(Status.Success);
+            AssertUnknownCase(Status.Unknown);
+            AssertInvalidCase(Status.Invalid);
         }
 
         [Fact]
         public void StatusValueOperations()
         {
-            AssertInvalidCase(Invalid + ResultCode.Success);
+            // Assert that 'Invalid' Status won't become successive
+            AssertInvalidCase(Status.Invalid + ResultCode.Success);
 
-            AssertSuccessCase(Unknown + Success);
+            // Assert an 'Unknown' Status later may change
+            AssertSuccessCase(Status.Unknown + Status.Success);
 
-            AssertUnknownCase(NoState + ResultCode.Unknown);
-            AssertUnknownCase(NoState + Unknown);
+            // Assert raising Status.Code via appending Status or ResultCode changes results  
+            AssertUnknownCase(Status.NoState + ResultCode.Unknown);
+            AssertUnknownCase(Status.NoState + Status.Unknown);
 
-            AssertNoStateCase(NoState.WithText("State of the Art"));
-            AssertNoStateCase(NoState.WithData("A fistful of Data"));
+            // Asserting weather appending message Text nor info Data may change a result 
+            AssertNoStateCase(Status.NoState.WithText("State of the Art"));
+            AssertNoStateCase(Status.NoState.WithData("A fistful of Data"));
 
-            Status Chicken  = NoState.WithText("Chicken or").WithData("Egg") + Unknown;
+            // Assert status 'Unknown' for the status of context related item data   
+            Status Chicken  = Status.NoState.WithText("Chicken or").WithData("Egg") + Status.Unknown;
             AssertUnknownCase( Chicken );
-            Assert.True( Chicken.ToString().EndsWith("Chicken or Egg"), Chicken );
-            
+            Assert.True( Chicken.ToString().EndsWith("Chicken or Egg"), Chicken.ToString() );
 
-            Status Banana = Chicken.WithText("Banana is") + ResultCode.Info + Invalid;
+            // Asserting correct behavior on status transition to a different context  
+            Status Banana = Chicken.WithText("Banana is") + ResultCode.Info + Status.Invalid;
             AssertInvalidCase( Banana );
-            Assert.False( !Banana.ToString().EndsWith("Egg"), Banana.ToString() );
-            Assert.False( Banana.Text.StartsWith("Banana"), Banana.ToString() ); 
+            Assert.True( Banana.ToString().EndsWith("Egg"), Banana.ToString() );
+            Assert.False( Banana.Text.StartsWith("Chicken"), Banana.ToString() ); 
             Assert.True( Banana.Text.Contains("Banana"), Banana.ToString() );
-            Banana = Chicken.WithText("Banana is") + Success.WithData("Gelb");
+
+            // Appending descriptive data to a status af transitioned contexts  
+            Banana = Chicken.WithText("Banana is") + Status.Success.WithData("Gelb");
             AssertSuccessCase( Banana );
             Assert.False(Banana.Text.StartsWith("Chicken"), Banana.ToString());
-            Assert.True(Banana.Data.ToString().Equals("Gelb"), Banana.ToString());
+            Assert.True(Banana.ToString().EndsWith("Gelb"), Banana.ToString());
         }
     }
 }
