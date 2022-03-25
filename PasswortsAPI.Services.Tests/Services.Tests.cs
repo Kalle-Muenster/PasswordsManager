bfe7fa3e066cf8a841168e03106987878ca3ad4c
Kalle Muenster <kalle.muenster@outlook.de>
2022-03-25 13:22:51 +0100

    Add test runs for Services + db states to be used

--- PasswortsAPI.Services.Tests/Services.Tests.cs
+++ PasswortsAPI.Services.Tests/Services.Tests.cs
@@ -53,6 +53,16 @@ namespace Services.Tests
             }
         }
 
+        
+        public static void AssertInvalidCase<M>(M modelclass) where M : EntityBase<M>, new()
+        {
+            Assert.False(modelclass, modelclass.ToString());
+            Assert.False(modelclass.IsValid(), modelclass.ToString());
+            Assert.False(modelclass.Waiting(), modelclass.ToString());
+            Assert.False(modelclass.Is().Status.Ok, modelclass.ToString());
+            Assert.True(modelclass.Is().Status.Bad, modelclass.ToString());
+        }
+
         public class Context : PasswordsApiDbContext<Context>
         {
             public static DbContextOptions<Context> CreateTestOptions( string database )
@@ -111,17 +121,205 @@ namespace Services.Tests
             }
         }
     }
+
+    public class PasswordUsersServiceTests
+    {
+
+        public void AssertUserAccount( PasswordUsers account, ITuple expects )
+        {
+            Assert.True( account.Id == (int)expects[0], "Id" );
+            Assert.True( account.Name.Equals(expects[1]), "Name" );
+            Assert.True( account.Mail.Equals(expects[2]), "Mail" );
+            Assert.True( account.Info.Equals(expects[3]), "Info" );
+        }
+
+        [Fact]
+        private void UserService_Assert_AccountCreation()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("UserService","EmptyDataBase");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            Status result = service.CreateNewAccount("ElNamo", "El@Mailo.de", "ElInfo").GetAwaiter().GetResult()
+            .Status;
+            PasswordUsers account = service.Entity;
+            
+            // Assert
+            Assert.False(result, result);
+            Assert.True(account.IsValid(), service.Entity.ToString());
+            AssertUserAccount(account, (1, "ElNamo", "El@Mailo.de", "ElInfo"));
+
+            // Close Session
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void UserService_Assert_AccountListing()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>( Test.CurrentContext );
+            
+            // Act
+            List<PasswordUsers> result = service.ListUserAccounts();
+
+            // Assert
+            Assert.True( (result != null), "result not null" );
+            Assert.True( result.Count == 1, result.Count.ToString() );
+            AssertUserAccount( result[0], (1,"ElNamo","El@Mailo.de","ElInfo") );
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void UserService_Assert_GetAccountById()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            PasswordUsers account = service.GetUserById(1).GetAwaiter().GetResult().Entity;
+
+            // Assert
+            Assert.True(account,account.ToString());
+            AssertUserAccount( account, (1, "ElNamo", "El@Mailo.de", "ElInfo") );
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void UserService_Asssert_GetAccountByName()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            PasswordUsers account = service.GetUserByNameOrId("ElNamo").GetAwaiter().GetResult().Entity;
+
+            // Assert
+            AssertUserAccount(account, (1, "ElNamo", "El@Mailo.de", "ElInfo"));
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void UserService_Assert_GetAccountByEmail()
+        {
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            PasswordUsers account = service.GetUserByEmail("El@Mailo.de").GetAwaiter().GetResult().Entity;
+
+            // Assert
+            AssertUserAccount(account, (1, "ElNamo", "El@Mailo.de", "ElInfo"));
+
+            Test.CurrentContext.Finished();
+        }
+
+
+        [Fact]
+        private void UserService_ErrorOn_GetAccountByInvalidId()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            PasswordUsers account = service.GetUserById(23).GetAwaiter().GetResult().Entity;
+
+            // Assert
+            Test.AssertInvalidCase( account );
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void UserService_ErrorOn_GetAccountByInvalidName()
+        {
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            PasswordUsers account = service.GetUserByNameOrId("Egon").GetAwaiter().GetResult().Entity;
+
+            // Assert
+            Assert.False( service.Status, service.Status.ToString() );
+            Test.AssertInvalidCase( account );
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void UserService_ErrorOn_GetAccountByInvalidEmail()
+        {
+            Test.Context.PrepareDataBase("UserService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> service = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+
+            // Act
+            PasswordUsers account = service.GetUserByEmail("El@Peilo.de").GetAwaiter().GetResult().Entity;
+
+            // Assert
+            Assert.False( service, service.ToString() );
+            Test.AssertInvalidCase( account );
+
+            Test.CurrentContext.Finished();
         }
     }
 
-    public class ServiceTests
+    public class UserPasswordsServiceTests
     {
-        [Fact] private void UserService()
+        [Fact]
+        private void Assert_CreatedUserAcount_Has_MasterPassword()
         {
-            PasswordUsersService<TestContext> users = new PasswordUsersService<TestContext>(new TestContext(null));
-            PasswordUsers account = users.ById(5).GetAwaiter().GetResult().Entity;
-            Assert.False( account.IsValid() );
-            Assert.True( account.Is().Status.Code.HasFlag( ResultCode.User ), account.ToString() );
+            // Arrange
+            Test.Context.PrepareDataBase("PasswordsService","OneUserNoPassword");
+            PasswordUsersService<Test.Context> usinger = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+            UserPasswordsService<Test.Context> service = new UserPasswordsService<Test.Context>(Test.CurrentContext,usinger);
+
+            // Act
+            Status result = service.SetMasterKey(1,"ElPasso").GetAwaiter().GetResult().Status;
+            UserPasswords usrkey = service.Entity;
+
+            // Assert
+            Assert.True(result.Text.Length>0, result);
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void Assert_PasswordVerification()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("PasswordsService","OneValidUserAccount");
+            PasswordUsersService<Test.Context> usinger = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+            UserPasswordsService<Test.Context> service = new UserPasswordsService<Test.Context>(Test.CurrentContext, usinger);
+
+            bool valid = service.VerifyPassword(1, "ElPasso");
+
+            Assert.True( valid , service.Status );
+
+            Test.CurrentContext.Finished();
+        }
+
+        [Fact]
+        private void Assert_GetMasterKey()
+        {
+            // Arrange
+            Test.Context.PrepareDataBase("PasswordsService","OneValidUserAccount");
+            PasswordUsersService<Test.Context> usinger = new PasswordUsersService<Test.Context>(Test.CurrentContext);
+            UserPasswordsService<Test.Context> service = new UserPasswordsService<Test.Context>(Test.CurrentContext, usinger);
+
+            Crypt.Key key = service.ForUserAccount( usinger.GetUserByNameOrId("ElNamo") ).GetAwaiter().GetResult().GetMasterKey(1);
+
+            Assert.True( service, service.Status.ToString() );
+            Assert.True( key.IsValid(), service.Status );
+            Assert.True( key.VerifyPhrase("ElPasso") );
+
+            Test.CurrentContext.Finished();
         }
     }
 }
