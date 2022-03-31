f049e1cf1c80484e10ba18f320fba671e39dc426
Kalle Muenster <kalle.muenster@outlook.de>
2022-03-31 18:39:50 +0200

    Rearrange and cleanup the controller

--- PasswordsAPI/Controllers/PasswordsController.cs
+++ PasswordsAPI/Controllers/PasswordsController.cs
@@ -34,49 +34,121 @@ namespace PasswordsAPI.Controllers
         }
 
 
-        [Produces( "application/json" ), HttpGet( "User")]
+        [Produces("application/json"), HttpPost("User")]
+        public async Task<IActionResult> NewUser(string name, string email, string pass)
+        {
+            PasswordUsers user = (await _usrs.CreateNewAccount(name, email, "")).Entity;
+            if ( user.IsValid() ) {
+                // as soon user has been add, set the users master password
+                // (or a password hash, if user keys residing client sided)
+                if ( await _keys.SetMasterKey( user.Id, pass ) ) {
+                    return new OkObjectResult( user );
+                } else {
+                    _db.PasswordUsers.Remove( user );
+                    _db.SaveChanges();
+                    return StatusCode( 500, _keys.Status.ToString() );
+                }
+            } else return StatusCode( 500, _usrs.Status.ToString() );
+        }
+
+        [Produces("application/json"), HttpGet("User")]
         public IActionResult GetUser()
         {
             return new OkObjectResult( _usrs.ListUserAccounts() );
         }
 
-        [Produces(  "application/json" ), HttpGet( "{user}/Info" )]
+        [Produces("application/json"), HttpPut("{user}/Info")]
+        public async Task<IActionResult> PutUserInfo(string user, string info)
+        {
+            if( (await _usrs.GetUserByNameOrId(user)).Entity.IsValid() ) {
+                PasswordUsers usr = _usrs.Entity;
+                usr.Info = info;
+                _db.Update(usr);
+                _db.SaveChanges();
+                return new OkObjectResult( usr );
+            } else return StatusCode( 400, _usrs.Status.ToString() );
+        }
+
+        [Produces("application/json"), HttpGet("{user}/Info")]
         public async Task<IActionResult> GetUserInfo(string user)
         {
-            if ( (await _usrs.GetUserByNameOrId( user )).Entity.Is().Status.Bad ) {
+            if ( (await _usrs.GetUserByNameOrId(user)).Entity.Is().Status.Bad ) {
                 return StatusCode( 400, _usrs.Status.ToString() );
             } else {
                 return Ok( _usrs.Entity.Info );
             }
         }
 
-        [Produces( "application/json" ), HttpPut( "{user}/Info" )]
-        public async Task<IActionResult> PutUserInfo(string user,string info)
+        [Produces("application/json"), HttpPut("{user}/Pass")]
+        public async Task<ActionResult> SetUserPassword(string user, string oldpass, string newpass)
         {
-            if ( (await _usrs.GetUserByNameOrId(user)).Entity.IsValid() ) {
-                PasswordUsers usr = _usrs.Entity;
-                usr.Info = info;
-                _db.Update( usr );
-                _db.SaveChangesAsync();
-                return new OkObjectResult( usr );
-            } else return StatusCode( 500, _usrs.Status.ToString() );
-        }
-
-        [Produces( "application/json" ), HttpPut( "{user}/Pass" )]
-        public async Task<ActionResult> SetUserPassword( string user, string oldpass, string newpass )
-        {
-            if( await _keys.ForUserAccount( _usrs.GetUserByNameOrId(user) ) ) {
-                if ( _keys.VerifyPassword( _usrs.Entity.Id, oldpass ) )
+            if (await _keys.LookupPasswordByUserAccount(_usrs.GetUserByNameOrId(user))) {
+                if (_keys.VerifyPassword(_usrs.Entity.Id, oldpass))
                     return new OkObjectResult(
                (await _keys.SetMasterKey(_usrs.Entity.Id, newpass)).Status.ToString()
                                                );
-            } return StatusCode(500, _keys.Status.ToString() );
-        }    
-        
+            } return StatusCode( 500, _keys.Status.ToString() );
+        }
+
+        [Produces("application/json"), HttpDelete("{user}")]
+        public async Task<IActionResult> RemoveUserAccount(string user, string mail, string pass)
+        {
+            if ((await _usrs.GetUserByNameOrId(user)).Entity) {
+                if (_usrs.Entity.Mail == mail) {
+                    if ((await _keys.LookupPasswordByUserAccount(_usrs.GetUserByNameOrId(user))).VerifyPassword(_usrs.Entity.Id, pass)) {
+                        if (await _usrs.RemoveUserAccount(_usrs.Entity)) return Ok( _usrs.Status.ToString() );
+                        return StatusCode(500, "Removing user account failed: " + _usrs.Status );
+                    } return StatusCode(303, _keys.Status );
+                } return StatusCode(404, _usrs.Status + "Incorrect Em@il address");
+            } return StatusCode(404, _usrs.Status );
+        }
+
+        [Produces("application/json"), HttpPost("{user}/Location")]
+        public async Task<IActionResult> NewUserLocation(string user, string name, string pass, string? login, string? info)
+        {
+            if ((await _usrs.GetUserByNameOrId(user)).Entity) {
+                UserLocations newArea = new UserLocations();
+                newArea.Area = name;
+                newArea.User = _usrs.Entity.Id;
+                newArea.Info = info ?? String.Empty;
+                newArea.Name = login ?? String.Empty;
+                await _locs.SetLocationPassword(_usrs.GetUserByNameOrId(user), newArea, pass);
+                if (_locs.GetLocationOfUser(newArea.User, newArea.Area).IsValid()) {
+                    return new OkObjectResult(_locs.Entity);
+                } else {
+                    return StatusCode(404, _locs.Status.ToString());
+                }
+            } else return StatusCode(404, _usrs.Status.ToString());
+        }
+
+        [Produces("application/json"), HttpGet("{user}/Locations")]
+        public async Task<IActionResult> GetUserLocations(string user) {
+            int usrid = _usrs.GetUserId(user);
+            if (usrid > 0) {
+                return new OkObjectResult( _locs.GetUserLocations(usrid) );
+            } else return StatusCode( 404, _usrs.Status.ToString() );
+        }
+
+        [Produces("application/json"), HttpGet("{user}/{area}")]
+        public async Task<IActionResult> GetUserLocation(string user, string area)
+        {
+            UserLocations location = _locs.GetLocationOfUser(_usrs.GetUserId(user), area);
+            if (location.IsValid()) return new OkObjectResult(location);
+            else return StatusCode(400, location.Is().Status.ToString());
+        }
+
+        [Produces("application/json"), HttpDelete("{user}/{area}")]
+        public async Task<IActionResult> RemoveLocation(string user, string area, string masterPass)
+        {
+            if (await _locs.RemoveLocation(_usrs.GetUserByNameOrId(user), area, masterPass))
+                return Ok($"Successfully removed password for: {area}");
+            else return StatusCode(500, _locs.Status.ToString());
+        }
+
         [Produces(  "application/json" ), HttpPut( "{user}/{area}/Login" )]
         public async Task<IActionResult> PutUserLocationLogin( string user, string area, string login )
         {
-            if( !await _locs.GetLocationEntity( _usrs.GetUserId(user), area) ) {
+            if( !_locs.GetLocationOfUser( _usrs.GetUserId(user), area) ) {
                 return StatusCode( 400, _locs.Status.ToString() );
             } else {
                 _locs.Entity.Name = login;
@@ -87,7 +159,7 @@ namespace PasswordsAPI.Controllers
         [Produces( "application/json" ), HttpPut( "{user}/{area}/Info" )]
         public async Task<IActionResult> PutUserLocationInfo( string user, string area, string info )
         {
-            UserLocations location = (await _locs.GetLocationEntity(_locs.GetAreaId(area, _usrs.GetUserId(user)))).Entity;
+            UserLocations location = (await _locs.GetLocationById(_locs.GetAreaId(area, _usrs.GetUserId(user)))).Entity;
             if ( !location.Is().Status.Bad ) {
                 location.Info = info;
                 _db.UserLocations.Update( location );
@@ -99,102 +171,28 @@ namespace PasswordsAPI.Controllers
         [Produces("application/json"), HttpPut("{user}/{area}/Password")]
         public async Task<IActionResult> PutUserLocationPassword(string user, string userPass, string area, string areaPass)
         {
-            if( await _locs.GetLocationEntity(_usrs.GetUserId(user), area ) ) {
-                if( await _locs.SetPassword(userPass, areaPass) ) {
-                    return Ok( _locs.Status.ToString() );
+            if (_locs.GetLocationOfUser(_usrs.GetUserId(user), area)) {
+                if (await _locs.SetPassword(userPass, areaPass)) {
+                    return Ok(_locs.Status.ToString());
                 }
-            } return StatusCode( 404, _locs.Status.ToString() );
+            } return StatusCode(404, _locs.Status.ToString());
         }
 
-        [Produces( "application/json"), HttpPost("User")]
-        public async Task<IActionResult> NewUser( string name, string email, string pass )
+        [Produces("application/json"), HttpGet("{user}/{area}/Password")]
+        public async Task<IActionResult> GetUserLocationPassword(string user, string area, string yps )
         {
-            PasswordUsers user = (await _usrs.CreateNewAccount( name, email,"" )).Entity;
-            if ( user.IsValid() ) {
-                // as soon user has been add, set the users master password
-                // (or a password hash, if user keys residing client sided)
-                if (await _keys.SetMasterKey( user.Id, pass )) {
-                    return new OkObjectResult( user );
-                } else {
-                    _db.PasswordUsers.Remove( user );
-                    _db.SaveChanges();
-                    return StatusCode( 500, _keys.Status.ToString() );
-                }
-            } else return StatusCode( 500, _usrs.Status.ToString() );
-        }
-
-        [Produces( "application/json" ), HttpGet( "{user}/Locations" )]
-        public async Task<IActionResult> GetUserLocations( string user )
-        {
-            int usrid = _usrs.GetUserId( user );
-            if (usrid > 0) {
-                return new OkObjectResult(_locs.GetUserLocations(usrid) );
-            } else return StatusCode( 404, _usrs.Status.ToString() );
-        }
-
-        [Produces( "application/json" ), HttpGet( "{user}/{area}" )]
-        public async Task<IActionResult> GetUserLocation( string user, string area )
-        {
-            UserLocations location = ( await _locs.GetLocationEntity(_usrs.GetUserId( user ), area ) ).Entity;
-            if (location.IsValid() ) return new OkObjectResult( location );
-            else return StatusCode( 400, location.Is().Status.ToString() );
-        }
-
-        [Produces( "application/json" ), HttpGet( "{user}/{area}/Password" )]
-        public async Task<IActionResult> GetUserLocationPassword( string user, string area, string crypt )
-        {
-            int userId = _usrs.GetUserId( user );
-            if (userId <= 0) return StatusCode(404, _usrs.Status.ToString());
-            if ( !(await _locs.GetLocationEntity( userId, area )) )
-                return StatusCode( 404,_locs.Status.ToString() );
-            Yps.CryptKey key = (await _keys.ForUserAccount(_usrs.GetUserById(userId))).GetMasterKey(userId);
-            if (!key.IsValid()) return StatusCode( 303, _keys.Status );
-            string master = key.Decrypt( crypt );
-            if ( master == null ) return StatusCode(303, "master password invalid" );
+            int userId = _usrs.GetUserId(user);
+            if (userId <= 0) return StatusCode( 404, _usrs.Status.ToString() );
+            if (!_locs.GetLocationOfUser(userId, area))
+                return StatusCode(404, _locs.Status.ToString());
+            Yps.CryptKey key = (await _keys.LookupPasswordByUserAccount(_usrs.GetUserById(userId))).GetMasterKey(userId);
+            if (!key.IsValid()) return StatusCode(303, _keys.Status);
+            string master = key.Decrypt( yps );
+            if (master == null) return StatusCode(303, "master password invalid");
             string pass = _locs.GetPassword( master.Substring(3) );
-            if ( _locs.Status.Bad ) {
-                return StatusCode( 303, _locs.Status.ToString()  );
+            if (_locs.Status.Bad) {
+                return StatusCode( 303, _locs.Status.ToString() );
             } return Ok( pass );
-        }
-
-        [Produces( "application/json" ), HttpPost( "{user}/Locations" )]
-        public async Task<IActionResult> NewUserLocation( string user, string name, string pass, string? login, string? info )
-        {
-            if( await _usrs.GetUserByNameOrId( user ) ) {
-                UserLocations newArea = new UserLocations();
-                newArea.Area = name;
-                newArea.User = _usrs.Entity.Id;
-                newArea.Info = info ?? String.Empty;
-                newArea.Name = login ?? String.Empty;
-                await _locs.SetLocationPassword(_usrs.GetUserByNameOrId(user), newArea, pass );
-                await _locs.GetLocationEntity( newArea.User, newArea.Area );
-                if( _locs.Entity.IsValid() ) {
-                    return new OkObjectResult( _locs.Entity );
-                } else {
-                    return StatusCode( 404, _locs.Status.ToString() ); }
-            } else
-                return StatusCode( 404, _usrs.Status.ToString() );
-        }
-
-        [Produces( "application/json" ), HttpDelete( "{user}/{area}" )]
-        public async Task<IActionResult> RemoveLocation( string user, string area, string masterPass )
-        {
-            if( await _locs.RemoveLocation( _usrs.GetUserByNameOrId( user ), area, masterPass ) )
-                return Ok( $"Successfully removed password for: {area}" );
-            else return StatusCode( 500, _locs.Status.ToString() );
-        }
-        
-        [Produces( "application/json" ), HttpDelete( "{user}" )]
-        public async Task<IActionResult> RemoveUserAccount( string user, string mail, string pass )
-        {
-            if ( await _usrs.GetUserByNameOrId( user ) ) {
-                if ( _usrs.Entity.Mail == mail ) {
-                    if ( (await _keys.ForUserAccount( _usrs.GetUserByNameOrId( user ) )).VerifyPassword( _usrs.Entity.Id, pass ) ) {
-                        if ( await _usrs.RemoveUserAccount(_usrs.Entity ) ) return new OkObjectResult( _usrs.Status.ToString() );
-                        else return StatusCode( 500, "removing user account failed" );
-                    } return StatusCode( 303, _keys.Status.ToString() );
-                } return StatusCode( 404, "incorrect Em@il address");
-            } return StatusCode( 404, _usrs.Status.ToString() );
         }
 
         [Produces("application/json"), HttpGet("errormessage/{code}")]
