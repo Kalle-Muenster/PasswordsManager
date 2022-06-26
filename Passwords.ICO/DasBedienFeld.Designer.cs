3d8f39497a68cdbb491a8ee786afd9934825050b
Kalle Muenster <kalle.muenster@outlook.de>
2022-06-26 19:46:49 +0200

    Add Passwords.ICO (Systray-icon project)

---
+++ Passwords.ICO/DasBedienFeld.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Passwords.ICO
+{
+    partial class DasBedienFeld
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_TheGUI = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btn_TheGUI
+            // 
+            this.btn_TheGUI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.btn_TheGUI.FlatAppearance.BorderColor = System.Drawing.Color.Wheat;
+            this.btn_TheGUI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_TheGUI.Location = new System.Drawing.Point(23, 21);
+            this.btn_TheGUI.Name = "btn_TheGUI";
+            this.btn_TheGUI.Size = new System.Drawing.Size(253, 37);
+            this.btn_TheGUI.TabIndex = 0;
+            this.btn_TheGUI.Tag = "Gui";
+            this.btn_TheGUI.Text = "ThePasswords - TheGUI";
+            this.btn_TheGUI.UseVisualStyleBackColor = false;
+            this.btn_TheGUI.Click += new System.EventHandler(this.BedienfeldButton_TheGUIClick);
+            this.btn_TheGUI.MouseEnter += new System.EventHandler(this.BedienfeldButton_OnHover);
+            this.btn_TheGUI.MouseLeave += new System.EventHandler(this.BedienfeldButton_OnHover);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(0, 0);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Click += new System.EventHandler(this.BedienfeldButton_StartClick);
+            this.btn_Start.MouseEnter += new System.EventHandler(this.BedienfeldButton_OnHover);
+            this.btn_Start.MouseLeave += new System.EventHandler(this.BedienfeldButton_OnHover);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(0, 0);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 0;
+            this.btn_Stop.Click += new System.EventHandler(this.BedienfeldButton_StopClick);
+            this.btn_Stop.MouseEnter += new System.EventHandler(this.BedienfeldButton_OnHover);
+            this.btn_Stop.MouseLeave += new System.EventHandler(this.BedienfeldButton_OnHover);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 0;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Location = new System.Drawing.Point(0, 0);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 24);
+            this.checkBox2.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // DasBedienFeld
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "DasBedienFeld";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn_TheGUI;
+        private Button btn_Start;
+        private Button btn_Stop;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Passwords.ICO/Program.cs
@@ -0,0 +1,121 @@
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace Passwords.ICO
+{            
+    internal class Program : IDisposable
+    { 
+        private NotifyIcon notifyIcon;
+        private ContextMenuStrip notificationMenu;
+        private System.ComponentModel.IContainer components;
+        private static DasBedienFeld panel;
+        private Commander commands;
+        private ToolStripMenuItem starter;
+        private ToolStripMenuItem stopper;
+
+        private bool disposing;
+        private void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) ) {
+                this.disposing = true;
+                components.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(!disposing);
+        }
+
+        private ToolStripMenuItem[] InitializeMenu()
+        {
+            System.Drawing.Image img = new Bitmap("karen96.ico");
+            ToolStripMenuItem[] menu = new ToolStripMenuItem[] {
+                new ToolStripMenuItem("Start TheGUI", img, MenuItem_Click),
+                new ToolStripMenuItem("Start Server", img, MenuItem_Click),
+                new ToolStripMenuItem("Stop Server", img, MenuItem_Click),
+                new ToolStripMenuItem("Exit Application", img, MenuItem_Click),
+            };
+            menu[0].Tag = Commands.StartTheGUI;
+            menu[1].Tag = Commands.StartServer;
+            menu[2].Tag = Commands.StopServing;
+            menu[3].Tag = Commands.ExitTheIcon;
+
+            starter = menu[1];
+            stopper = menu[2];
+
+            return menu;
+        }
+
+        private void MenuItem_Click( object sender, EventArgs e )
+        {
+            Commands com = (Commands)(sender as ToolStripMenuItem).Tag;
+            if( commands[com].CanExecute )
+                commands[com].Execute();
+        }
+
+        private void ApplicationExitClick()
+        {
+            if( commands[Commands.StopServing].CanExecute )
+                commands[Commands.StopServing].Execute();
+            Thread.Sleep(2000);
+            Application.Exit();
+        }
+
+        private void IconMouseClick( object sender, MouseEventArgs e )
+        {
+            if( e.Button == MouseButtons.Left ) {
+                if( panel == null )
+                    panel = new DasBedienFeld(commands);
+                if( panel.Visible )
+                    panel.Hide();
+                else {
+                    panel.Show();
+                    panel.Location = new Point(1600, 550);
+                }
+            }
+        }
+
+        public Program()
+        {
+            commands = new Commander( new Command( Commands.ExitTheIcon, ApplicationExitClick, ()=>{return true;} ));
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            components = new System.ComponentModel.Container();
+            notifyIcon = new NotifyIcon(components);
+            notificationMenu = new ContextMenuStrip(components);
+            notificationMenu.Items.AddRange( InitializeMenu() );
+            notificationMenu.Opened += NotificationMenu_Opened;
+            notifyIcon.ContextMenuStrip = notificationMenu;
+            notifyIcon.Icon = new Icon("karen96.ico");
+            notifyIcon.MouseClick += IconMouseClick;
+            components.Add( notifyIcon );
+        }
+
+        private void NotificationMenu_Opened( object? sender, EventArgs e )
+        {
+            starter.Enabled = commands[Commands.StartServer].CanExecute;
+            stopper.Enabled = commands[Commands.StopServing].CanExecute;
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            // Consola.StdStream.Init();
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            Program prog = new Program();
+            prog.notifyIcon.Visible = true;
+            Application.Run();
+            prog.notifyIcon.Dispose();
+        }
+
+
+    }
+}
